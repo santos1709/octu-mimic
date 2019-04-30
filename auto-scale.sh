184235cc8985839ec0b7addab4b7e6312a055cff
Guilherme Santos <santos1709@gmail.com>
2019-04-29 21:12:56 -0300

    docker files configured

--- auto-scale.sh
+++ auto-scale.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-echo "  watcher-service:" > ././../docker-compose.yml
-echo "    build: ./watcher" >> ././../docker-compose.yml
-echo "    volumes:" >> ././../docker-compose.yml
-echo "      - ./watcher:/usr/src/app" >> ././../docker-compose.yml
-echo "    ports:" >> ././../docker-compose.yml
-echo "      - 5100:8080" >> ././../docker-compose.yml
-echo "    environment:" >> ././../docker-compose.yml
-echo "      - CALLCOUNT" >> ././../docker-compose.yml
-echo "" >> ././../docker-compose.yml
-
 for i in {1..$1}
 do
     echo "  api-service-$i:" >> ././../docker-compose.yml
